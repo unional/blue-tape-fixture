4ee56dd6a570c438d23e66258fc0949f0d05464b
Homa Wong <homawong@gmail.com>
2016-06-19 01:49:17 -0700

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
 import test = require('blue-tape');
 import { join } from 'path';
+
 export interface FixtureTest {
   (name: string, fixtureName: string, cb: (t: test.Test, path: string) => any): test.Test;
   only(name: string, fixtureName: string, cb: (t: test.Test, path: string) => any): test.Test;
   skip(name: string, fixtureName: string, cb: (t: test.Test, path: string) => any): test.Test;
 }
 
-export default function fixture(path: string): FixtureTest {
+/**
+ * Create fixture test with the specified fixture base path.
+ * @param fixtureBasePath Fixture base path. If using relative path, starts at the project root.
+ */
+export default function fixture(fixtureBasePath: string): FixtureTest {
   function curry(testfn: (name: string, cb: test.TestCase) => any): any {
     return (
       title: string,
       fixtureName: string,
       cb: (t: test.Test, path: string) => any
     ) => {
-      const fixturePath = join(path, fixtureName);
+      const fixturePath = join(fixtureBasePath, fixtureName);
       return testfn(`${title} (fixture: ${fixtureName})`, t => {
         return cb(t, fixturePath);
       });
--- test/index.ts
+++ test/index.ts
@@ -1,8 +1,11 @@
+import { join } from 'path';
+import { existsSync } from 'fs';
 import fixture from '../src/index';
 
-const test = fixture('fixtures');
+const test = fixture('test/fixtures');
 
 test('running', 'case-1', (t, path) => {
-  t.equal(path, 'fixtures/case-1');
+  t.equal(path, 'test/fixtures/case-1', 'path should be "test/fixtures/case-1"');
+  t.assert(existsSync(join(path, 'somefile.txt')), 'should find somefile.txt');
   t.end();
 });
