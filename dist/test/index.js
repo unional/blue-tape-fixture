375d95eff2f87e8d1d7bdab7b806e8d77c6559a2
Homa Wong <homawong@gmail.com>
2016-07-12 00:18:08 -0700

    Update test

--- dist/test/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var blue_tape_fixture_1 = require('blue-tape-fixture');
-var test = blue_tape_fixture_1.default('fixtures');
-test('running', 'case-1', function (t, path) {
-    t.equal(path, 'fixtures/case-1');
-    t.end();
-});
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,10 +2,18 @@ import { join } from 'path';
 import fixture from './index';
 import { existsSync } from 'fs';
 
-const test = fixture('test/fixtures');
+const test = fixture(join(__dirname, 'fixtures'));
 
-test('running', 'case-1', (t, path) => {
-  t.equal(path, 'test/fixtures/case-1', 'path should be "test/fixtures/case-1"');
-  t.assert(existsSync(join(path, 'somefile.txt')), 'should find somefile.txt');
+test('abs path', 'case-1', (t, path) => {
+  const filePath = join(path, 'somefile.txt');
+  t.assert(existsSync(filePath), 'should find somefile.txt');
+  t.end();
+});
+
+const rtest = fixture('src/fixtures');
+
+rtest('relative path', 'case-1', (t, path) => {
+  const filePath = join(path, 'somefile.txt');
+  t.assert(existsSync(filePath), 'should find somefile.txt');
   t.end();
 });
